8050ea21d8b3dd193dd5b6344334c811c26d0250
jakkujakku <jakkujakku98@gmail.com>
2023-12-01 16:27:45 +0900

    [FEAT]: SwiftUI Tutorial - Passing data with bindings

---
+++ SwiftUI/PassingDataWithBindings/StartingProject/Scrumdinger/Views/ThemePicker.swift
@@ -0,0 +1,18 @@
+//
+//  ThemePicker.swift
+//  Scrumdinger
+//
+//  Created by 준우의 MacBook 16 on 12/1/23.
+//
+
+import SwiftUI
+
+struct ThemePicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ThemePicker()
+}
