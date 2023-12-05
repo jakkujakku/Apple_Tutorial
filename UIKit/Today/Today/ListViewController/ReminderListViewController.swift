126a83e76aae1c1446e32de1937e3faa58553e20
jakkujakku <jakkujakku98@gmail.com>
2023-12-06 02:52:40 +0900

    [FEAT]: UIKit Tutorial - Displaying cell info

--- UIKit/Today/Today/ListViewController/ReminderListViewController.swift
+++ UIKit/Today/Today/ListViewController/ReminderListViewController.swift
@@ -7,10 +7,15 @@
 
 import UIKit
 
-class ReminderListViewController: UICollectionViewController {
-    typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
-    typealias Snapshot = NSDiffableDataSourceSnapshot<Int, String>
+/*
+ The following process for building a collection view and filling it with items
+ 1. Create a data source for your collection view.
+ 2. Implement a cell provider that configures your collection view’s cells.
+ 3. Generate the current state of the data.
+ 4. Display the data in the user interface.
+ */
 
+class ReminderListViewController: UICollectionViewController {
     var dataSource: DataSource!
 
     override func viewDidLoad() {
@@ -19,12 +24,7 @@ class ReminderListViewController: UICollectionViewController {
         let listLayout = listLayout()
         collectionView.collectionViewLayout = listLayout
 
-        let cellRegistration = UICollectionView.CellRegistration {
-            (cell: UICollectionViewListCell, indexPath: IndexPath, _: String) in
-            let reminder = Reminder.sampleData[indexPath.item]
-            var contentConfiguration = cell.defaultContentConfiguration()
-            cell.contentConfiguration = contentConfiguration
-        }
+        let cellRegistration = UICollectionView.CellRegistration(handler: cellRegistrationHandler)
 
         dataSource = DataSource(collectionView: collectionView) { (collectionView: UICollectionView, indexPath: IndexPath, itemIdentifier: String) in
             return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: itemIdentifier)
